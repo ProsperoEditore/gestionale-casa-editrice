9f7cbe05aab8de67b393eacf0d2c6170acafeaaf
Prospero <info@prosperoeditore.com>
2025-04-15 18:49:46 +0200

    Fix migrazione: versionata con controlli su colonne mancanti

--- database/migrations/2025_03_28_134003_remove_old_royalties_columns_from_contratti_table.php
+++ database/migrations/2025_03_28_134003_remove_old_royalties_columns_from_contratti_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RemoveOldRoyaltiesColumnsFromContrattiTable extends Migration
+class RemoveOldRoyaltiesColumnsFromContrattiTableV2 extends Migration
 {
     public function up()
     {
