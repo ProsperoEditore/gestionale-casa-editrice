eebfec7b1090de59b12330e9831acb72290351a4
Prospero <info@prosperoeditore.com>
2025-04-15 23:53:21 +0200

    Fix percorso manifest.json (fuori da .vite)

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,19 @@ import path from 'path';
 export default defineConfig({
     build: {
         manifest: true,
-        outDir: 'public/build',
+        outDir: 'public/build', // Dove generare i file
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                style: 'resources/css/app.css',
+            },
+            output: {
+                // NIENTE .vite! Qui forziamo tutto in public/build
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+            },
+        },
         emptyOutDir: true,
     },
     plugins: [
