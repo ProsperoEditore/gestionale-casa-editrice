0e1ea79b05c069be8ca93a2d687b7be3853c60e0
Prospero <info@prosperoeditore.com>
2025-04-15 23:44:27 +0200

    Corregge path manifest Vite per Heroku

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        https: true,
-    },
-    build: {
-        manifest: true,
-        outDir: 'public/build',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
     },
 });
