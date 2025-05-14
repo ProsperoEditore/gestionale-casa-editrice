efddfb554a2a63e8fe7ebfc99155d9175f103928
Prospero <info@prosperoeditore.com>
2025-05-14 16:09:51 +0200

    salvataggio righe singole nelle giacenze

--- resources/views/giacenze/create.blade.php
+++ resources/views/giacenze/create.blade.php
@@ -43,7 +43,19 @@
         </thead>
         <tbody id="giacenzeTableBody">
         @foreach ($giacenze as $giacenza)
-            <tr data-id="{{ $giacenza->id }}">
+                        <tr data-id="{{ $giacenza->id }}" data-original='@json([
+                "isbn" => $giacenza->isbn,
+                "titolo" => $giacenza->titolo,
+                "quantita" => $giacenza->quantita,
+                "prezzo" => $giacenza->prezzo,
+                "note" => $giacenza->note,
+                $magazzino->anagrafica->categoria === 'magazzino editore'
+                    ? "costo_produzione" : "sconto"
+                    => $magazzino->anagrafica->categoria === 'magazzino editore'
+                        ? $giacenza->costo_produzione
+                        : $giacenza->sconto,
+            ])'>
+
                 <td><input type="text" class="form-control marchio" value="{{ $giacenza->libro->marchio_editoriale->nome ?? 'N/D' }}" readonly></td>
                 <td><input type="text" class="form-control isbn" value="{{ $giacenza->isbn }}" readonly></td>
                 <td><input type="text" class="form-control titolo" value="{{ $giacenza->titolo }}"></td>
