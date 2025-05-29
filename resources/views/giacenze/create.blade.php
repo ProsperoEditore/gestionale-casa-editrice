8ea19518cb9824acf6bcee0ed5c2ca109ae3fc14
Prospero <info@prosperoeditore.com>
2025-05-29 11:43:55 +0200

    salva singola riga in giacenze

--- resources/views/giacenze/create.blade.php
+++ resources/views/giacenze/create.blade.php
@@ -203,10 +203,23 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 
 
 <script>
 document.addEventListener("DOMContentLoaded", function() {
+    const table = $('#giacenzeTable').DataTable({
+        paging: true,
+        info: false,
+        searching: false,
+        ordering: false,
+        lengthChange: false,
+        drawCallback: function () {
+            aggiornaTotaliGiacenze();
+        }
+    });
+
     aggiornaTotaliGiacenze();
     let libri = @json($libri);
 
