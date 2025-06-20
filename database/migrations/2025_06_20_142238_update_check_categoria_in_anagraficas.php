95e929fb5d91afc7381acb5c6d71597ff9570812
Prospero <info@prosperoeditore.com>
2025-06-20 16:29:48 +0200

    fix migr

--- database/migrations/2025_06_20_142238_update_check_categoria_in_anagraficas.php
+++ database/migrations/2025_06_20_142238_update_check_categoria_in_anagraficas.php
@@ -1,3 +1,5 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
@@ -5,10 +7,8 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // Rimuove la vecchia constraint
         DB::statement("ALTER TABLE anagraficas DROP CONSTRAINT IF EXISTS anagraficas_categoria_check");
 
-        // Aggiunge la nuova constraint con 'scuola'
         DB::statement("
             ALTER TABLE anagraficas
             ADD CONSTRAINT anagraficas_categoria_check
@@ -35,7 +35,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        // Ripristina la constraint originale (senza 'scuola')
         DB::statement("ALTER TABLE anagraficas DROP CONSTRAINT IF EXISTS anagraficas_categoria_check");
 
         DB::statement("
