12218d77dcd4cc810401f412c17db5b40b9bf38d
Prospero <info@prosperoeditore.com>
2025-06-24 15:06:29 +0200

    fix profilo

--- resources/views/profilo/partials/fatturazione.blade.php
+++ resources/views/profilo/partials/fatturazione.blade.php
@@ -34,7 +34,28 @@
         </div>
         <div class="col-md-6">
             <label class="form-label">Regime fiscale</label>
-            <input type="text" name="regime_fiscale" class="form-control" value="{{ old('regime_fiscale', $profilo->regime_fiscale ?? 'Editoria (art.74, c.1, DPR 633/72)') }}">
+            <select name="regime_fiscale" class="form-select" required>
+                <option value="">Seleziona</option>
+                <option value="RF01" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF01' ? 'selected' : '' }}>Ordinario</option>
+                <option value="RF02" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF02' ? 'selected' : '' }}>Contribuenti minimi (art.1, c.96-117, L. 244/07)</option>
+                <option value="RF04" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF04' ? 'selected' : '' }}>Agricoltura e attività connesse e pesca (artt.34 e 34-bis, DPR 633/72)</option>
+                <option value="RF05" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF05' ? 'selected' : '' }}>Vendita sali e tabacchi (art.74, c.1, DPR 633/72)</option>
+                <option value="RF06" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF06' ? 'selected' : '' }}>Commercio fiammiferi (art.74, c.1, DPR 633/72)</option>
+                <option value="RF07" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF07' ? 'selected' : '' }}>Editoria (art.74, c.1, DPR 633/72)</option>
+                <option value="RF08" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF08' ? 'selected' : '' }}>Gestione servizi telefonia pubblica (art.74, c.1, DPR 633/72)</option>
+                <option value="RF09" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF09' ? 'selected' : '' }}>Rivendita documenti di trasporto pubblico e di sosta</option>
+                <option value="RF10" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF10' ? 'selected' : '' }}>Intrattenimenti, giochi e altre attività di cui alla tariffa allegata al DPR 640/72</option>
+                <option value="RF11" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF11' ? 'selected' : '' }}>Agenzie viaggi e turismo (art.74-ter, DPR 633/72)</option>
+                <option value="RF12" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF12' ? 'selected' : '' }}>Agriturismo (art.5, c.2, L. 413/91)</option>
+                <option value="RF13" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF13' ? 'selected' : '' }}>Vendite a domicilio (art.25-bis, c.6, DPR 600/73)</option>
+                <option value="RF14" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF14' ? 'selected' : '' }}>Beni usati, oggetti d’arte, antiquariato o da collezione (art.36, DL 41/95)</option>
+                <option value="RF15" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF15' ? 'selected' : '' }}>Agenzie di vendita all’asta di oggetti d’arte, antiquariato o da collezione</option>
+                <option value="RF16" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF16' ? 'selected' : '' }}>IVA per cassa P.A. (art.6, c.5, DPR 633/72)</option>
+                <option value="RF17" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF17' ? 'selected' : '' }}>IVA per cassa (art. 32-bis, DL 83/2012)</option>
+                <option value="RF18" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF18' ? 'selected' : '' }}>Altro</option>
+                <option value="RF19" {{ old('regime_fiscale', $profilo->regime_fiscale ?? '') == 'RF19' ? 'selected' : '' }}>Regime forfettario (art.1, c.54-89, L. 190/2014)</option>
+            </select>
+
         </div>
         <div class="col-md-12">
             <label class="form-label">IBAN</label>
