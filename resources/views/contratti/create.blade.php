62a6a188681c01284b45f7a3e434e96d098c6659
Prospero <info@prosperoeditore.com>
2025-07-02 18:40:59 +0200

    edit contratti

--- resources/views/contratti/create.blade.php
+++ resources/views/contratti/create.blade.php
@@ -7,7 +7,7 @@
     <div class="card">
         <div class="card-body">
             <form action="{{ route('contratti.store') }}" method="POST">
-                @include('contratti._form')
+                @include('contratti._form', ['contratto' => null])
 
                 <div class="text-center mt-3">
                     <button type="submit" class="btn btn-primary">Salva</button>
