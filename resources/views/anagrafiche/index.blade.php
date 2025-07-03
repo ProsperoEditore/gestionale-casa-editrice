df04d5d355c36feba41e2d07f876fe332a78372e
Prospero <info@prosperoeditore.com>
2025-07-03 16:37:30 +0200

    fix cerca anagr

--- resources/views/anagrafiche/index.blade.php
+++ resources/views/anagrafiche/index.blade.php
@@ -102,5 +102,22 @@
         {{ $items->appends(request()->query())->onEachSide(1)->links('pagination::bootstrap-5') }}
     </div>
 </div>
+@endsection
 
+@section('scripts')
+    <!-- jQuery (se non già incluso) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Select2 CSS & JS -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#anagrafica_search').select2({
+                placeholder: 'Cerca per anagrafica...',
+                allowClear: true,
+                width: 'resolve'
+            });
+        });
+    </script>
 @endsection
