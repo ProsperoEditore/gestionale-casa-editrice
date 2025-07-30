b53db75318211d367ffab1ed5780c2bba0d886ef
Prospero <info@prosperoeditore.com>
2025-07-30 11:34:14 +0200

    fix autori

--- resources/views/ritenute/edit.blade.php
+++ resources/views/ritenute/edit.blade.php
@@ -7,9 +7,9 @@
     <div class="alert alert-warning mt-3" role="alert">
     <strong>Nota:</strong> per ottenere un <strong>Netto da pagare</strong> specifico, inserire nel campo <strong>Importo</strong> il valore risultante da:
     <ul class="mb-0 mt-2">
-        <li><strong>Netto × 95%</strong> per autori extracomunitari</li>
-        <li><strong>Netto × 85%</strong> per autori under 35</li>
-        <li><strong>Netto × 88%</strong> per autori over 35</li>
+        <li><strong>Netto : 95%</strong> per autori extracomunitari</li>
+        <li><strong>Netto : 85%</strong> per autori under 35</li>
+        <li><strong>Netto : 88%</strong> per autori over 35</li>
     </ul>
 </div>
 
