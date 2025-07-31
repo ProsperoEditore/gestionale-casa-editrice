3acdfd941e03b5ee6c04253d5126752350ffae01
Prospero <info@prosperoeditore.com>
2025-07-31 12:22:28 +0200

    fix autori

--- resources/views/autori/form.blade.php
+++ resources/views/autori/form.blade.php
@@ -105,7 +105,7 @@
 @endsection
 
 @push('scripts')
-<!-- jQuery UI se non già incluso altrove -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
 
 <script>
